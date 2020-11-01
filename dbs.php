4f51ea42c9818e2801404dd28f03a4455742c27f
ClaudioPaul <73767843+ClaudioPaul@users.noreply.github.com>
2020-10-31 23:40:45 -0500

    dbs

---
+++ dbs.php
@@ -0,0 +1,68 @@
+<html>
+	<title>DRAGON BALL SUPER</title>
+	<header>
+	<h1 align = center> DRAGON BALL SUPER TODOS LOS CAPÍTULOS</h1>
+	<table border=  5px >
+	<tr>
+	<td>
+			<h3 align="center">Capítulo 1</h3>
+	<iframe width="627" height="309" src="http://s02-stream.solidfilesusercontent.com/stream/YWJhMTRjYThjZDM2MjQ5NTA0MzBmMDM5ZDQwMjY3MGNlMmMxNzBhZDoxa1ozTGc6UWozSjU2ejVkOUxHRE9aRnNOblVPdmRueDlF/R4D4MNnpdn8dZ.mp4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	</td>
+	<td>
+			<h3 align="center">Capítulo 2</h3>
+	<iframe width="627" height="309" src="http://s01-stream.solidfilesusercontent.com/stream/YWU2OTNjNzdlYzM5MzA1NjhiYjUwMzdjMDg1NTZiZWY4YTA5OWNiYzoxa1oycUU6UVQ1Rlg1MjZyMXNXOGQzN0ctLVZSeGtYQVVn/nDKDRWVXLDZAk.mp4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	</td>
+	</tr>
+	<tr>
+	<td>
+			<h3 align="center">Capítulo 3</h3>
+	<iframe width="627" height="309" src="http://s01-stream.solidfilesusercontent.com/stream/MGFlZDZiODU1ZjBjOWMyZDM2NDEyNzc1NWRhMDc0MmQzZDg5ZmEyNDoxa1oycWs6ZGp4S0lscjJrU2piM2h3TEVrSXN4Y1plVEFJ/azLzjm225Nyg3.mp4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	</td>
+	<td>
+			<h3 align="center">Capítulo 4</h3>
+	<iframe width="627" height="309" src="http://s02-stream.solidfilesusercontent.com/stream/ZjJjNWNhZGRkYTIxMjI5YTE0MGQ0NjYwNDdkNDMwZjY2YThkNmVlZDoxa1oycnc6ejVIUjRRcjZsNTM1WnVVbEV3R2FlazZWeFZn/MDKDpA6Myvnnx.mp4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	</td>
+	</tr>
+	<tr>
+	<td>
+			<h3 align="center">Capítulo 5</h3>
+	<iframe width="627" height="309" src="http://s02-stream.solidfilesusercontent.com/stream/MGZkYmE5YmEwY2I4ZWFhYjE3OGIyZDYyYjdmZjE3ZDFjODRmYmNmMzoxa1oyc086YTk3aHF0ZWhWU0ZDdWZOYnZSSkJVTWFCTF9n/e6m6525Gn2VeB.mp4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	</td>
+	<td>
+			<h3 align="center">Capítulo 6</h3>
+	<iframe width="627" height="309" src="http://s02-stream.solidfilesusercontent.com/stream/NDQzMjBjZWNjMWZhN2U5OGZjNDJjZDkyN2IxNmU0NDU3OGZmZWNjNToxa1oydEY6S0JvMlNpNElXQlJQV1QxYzlfNHdjMjdtdkhJ/azLzjAdvyXPxL.mp4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	</td>
+	</tr>
+	<tr>
+	<td>
+			<h3 align="center">Capítulo 7</h3>
+	<iframe width="627" height="309" src="http://s01-stream.solidfilesusercontent.com/stream/NTEzYTFmYzQxMzhjOWQ3ZGI4Yzk0NTliYzIzNjI5ODY2ZTcwYTY5MToxa1oyeXk6QzNEU3UwenFjalN5bzBxSmI2MjZIU2kwWkJB/7GLGKmL2mRdjB.mp4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	</td>
+	<td>
+			<h3 align="center">Capítulo 8</h3>
+	<iframe width="627" height="309" src="http://s01-stream.solidfilesusercontent.com/stream/ZjM4MTE4MThmMDMxZmJlZThkZDQwOWU2NjQ5MWI5OWY0YzU4Mzk5YToxa1ozcHM6YUM1Y0RNN2FLSkNxNm5QQlg0MHBxOTM4ZGRv/88z8xVzGpL87j.mp4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	</td>
+	</tr>
+	<tr>
+	<td>
+			<h3 align="center">Capítulo 9</h3>
+	<iframe width="627" height="309" src="http://s01-stream.solidfilesusercontent.com/stream/NDhlMjRiNmMxNWYzMjE2NmU3Yjc3ZmI2MTI3ODdlYjc1NWEyYmJjODoxa1ozejQ6M1FpZUxodFY2YlFqM3Q5Y0liZXlfSnNxck9n/YqkqmerrPkzpq.mp4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	</td>
+	<td>
+			<h3 align="center">Capítulo 10</h3>
+	<iframe width="627" height="309" src="http://s01-stream.solidfilesusercontent.com/stream/MDFlZDYxOGMyY2E5ZDA0OWEyM2NjOWY0N2IxMDBiZDQ0YmJmMmRlYjoxa1o0M3A6c0ZHdHI2TkJzbVBSMmFHVkVaT1ZUaHdaQ1JJ/K3N3a2MYNrPpq.mp4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	</td>
+	</tr>
+	<tr>
+	<td>
+			<h3 align="center">Capítulo 11</h3>
+	<iframe width="627" height="309" src="http://s02-stream.solidfilesusercontent.com/stream/NDU1YmU3M2ZlMDIzYzFiYTAyYjJkYzg1ZDhiZDI5ZWIyNmVmYmM4OToxa1o0QXY6T0VLcHF4b0NoN1dORUtQQzAtNXFtemx1QWlB/MDKDRnmLVvdx8.mp4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	</td>
+	<td>
+			<h3 align="center">Capítulo 12</h3>
+	<iframe width="627" height="309" src="http://s01-stream.solidfilesusercontent.com/stream/MmNjOGZiY2RmOGIwMTg1NzFmMWEzYTk2ZTk5NzFmNTc1YmNkMDI4Zjoxa1o0Qlc6VTdwSlFHdHhqUHhROFBJSVdTWlhjSnBlR2Iw/YqkqNnpBkqm8K.mp4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	</td>
+	</tr>
+	</table>
+	</header>
+</html>
